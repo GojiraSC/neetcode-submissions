144693098cdda3391ab296944a98ad2a157cb15f
Austin <163770150+GojiraSC@users.noreply.github.com>
2026-04-17 18:21:20 +0000

    Add: python-more-operators - submission-5

---
+++ Python For Beginners/python-more-operators/submission-5.py
@@ -0,0 +1,11 @@
+a, b, c = 2, 8, 5
+
+prod_result = ((a * b) // c)
+rem_result = ((a * b) % c)
+result_1 = a ** b
+result_2 = b ** c
+
+print(prod_result)
+print(rem_result)
+print(result_1)
+print(result_2)
